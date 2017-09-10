a29731bd4e7d9b3895119f2b5804d1c5a1ca160d
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-10 18:36:42 +0100

    added the other operators and working on bugs with selecting invalid operators and making the script repeat itself.

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ console.log('hi im adam')
 var operator = prompt ('Choose your operator:');
 
 
-if (operator !== ('+' || '-' || '/' || '*')) {
-	var operator = prompt('Invalid operator! Please choose a correct operator:')
-}
-
 if (operator === '+') {
 
 	var numberOne = prompt ('Choose your first number:');
@@ -21,7 +17,59 @@ if (operator === '+') {
 	alert(alertInput);
 	console.log(alertInput);
 
-	var operator = prompt('Choose your operator:')
+}
+
+if (operator === '-') {
+
+	var numberOne = prompt ('Choose your first number:');
+	numberOne = parseFloat(numberOne);
+	var numberTwo = prompt ( 'Choose your second number');
+	numberTwo = parseFloat(numberTwo);
+
+	var numberThree = (numberOne - numberTwo);
+	console.log(numberThree);
+
+	var alertInput = numberOne + ' - ' + numberTwo + ' = ' + numberThree;
+	alert(alertInput);
+	console.log(alertInput);
 
 }
 
+if (operator === '*') {
+
+	var numberOne = prompt ('Choose your first number:');
+	numberOne = parseFloat(numberOne);
+	var numberTwo = prompt ( 'Choose your second number');
+	numberTwo = parseFloat(numberTwo);
+
+	var numberThree = (numberOne * numberTwo);
+	console.log(numberThree);
+
+	var alertInput = numberOne + ' * ' + numberTwo + ' = ' + numberThree;
+	alert(alertInput);
+	console.log(alertInput);
+
+}
+
+if (operator === '/') {
+
+	var numberOne = prompt ('Choose your first number:');
+	numberOne = parseFloat(numberOne);
+	var numberTwo = prompt ( 'Choose your second number');
+	numberTwo = parseFloat(numberTwo);
+
+	var numberThree = (numberOne / numberTwo);
+	console.log(numberThree);
+
+	var alertInput = numberOne + ' / ' + numberTwo + ' = ' + numberThree;
+	alert(alertInput);
+	console.log(alertInput);
+
+}
+
+
+if (operator !== ('+' || '-' || '/' || '*')) {
+	var operator = prompt('Invalid operator! Please choose a correct operator:')
+}
+
+var operator = prompt('Choose your operator')
